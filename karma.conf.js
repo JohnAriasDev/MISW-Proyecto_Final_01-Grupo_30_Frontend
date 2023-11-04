417f19ea76d7aa969a63ba6469e7eade8a2c5172
John Arias <ja.ariasa1@uniandes.edu.co>
2023-11-04 12:27:55 -0500

    modify sonar properties

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// Karma configuration file
 
 module.exports = function (config) {
   config.set({
@@ -15,23 +14,30 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // leave Jasmine Spec Runner output visible in the browser
+    },
+    coverageIstanbulReporter: {
+      reports: ['lcovonly', 'html'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['mocha', 'kjhtml', 'sonarqubeUnit', 'coverage'],
+    preprocessors: {
+      './src/**/*.ts': ['coverage']
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/front'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'lcovonly' }]
     },
     sonarQubeUnitReporter: {
-          sonarQubeVersion: 'LATEST',
-          outputFile: 'reports/ut_report.xml',
-          overrideTestDescription: true,
-          testPaths: ['./src'],
-          testFilePattern: '.spec.ts',
-          useBrowserName: false
-     },
-    reporters: ['mocha', 'kjhtml', 'sonarqubeUnit', 'coverage'],
-    port: 4200,
+      sonarQubeVersion: 'LATEST',
+      outputFile: 'reports/ut_report.xml',
+      overrideTestDescription: true,
+      testPaths: ['./src'],
+      testFilePattern: '.spec.ts',
+      useBrowserName: false
+    },
+    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
